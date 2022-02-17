85bf7e385d30c0302a89aa82695f65231dc1792b
Gerry Chen <gerry.chen2015@gmail.com>
2022-02-17 10:19:18 -0500

    Add version bump instructions
    Add some short instructions since each time I want to do a version bump I forget the exact process.

--- package.sh
+++ package.sh
@@ -1,3 +1,13 @@
+# Version Bump Instructions:
+# 0. Create release/vX.X.X branch
+# 1. Find-all-replace old version number with new version number (except in CHANGELOG file), update CHANGELOG, and push to github (needed for activelinks.json)
+# 2. Run `web-ext build -i data icons/outline.sh package.sh` (note: `-i` flag means "ignore") (or maybe just do step 4 instead?)
+# 3. Upload .zip to Firefox add-on developer dashboard to get a .xpi file and save it to the `web-ext-artifacts` folder.
+# 4. Run this script.  (It's ok if you run it before steps 0-3; it won't delete anything important, just run it again now)
+# 5. Test zip file in chrome at a minimum.  If everything works well, tag this commit, `git push --tags`, and merge back into master.
+# 6. Create version on github.  Upload the 3 github-artifacts.
+# 7. Upload to chrome, firefox, and edge stores.  Badges should update automatically.
+
 version=$(gsed -n 's/^.*"version": "\(.*\)",/\1/p' manifest.json)
 echo "Version: $version"
 fname="web-ext-artifacts/sci-hub_x_now_-${version}.zip"
